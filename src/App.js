db87c222ca34da9ee18d99ea43992e4fe26a1f33
ubuntu user <ubuntu@AZEGHERU-RO.ro.oracle.com>
2019-10-04 17:57:44 +0300

    Changed home page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed with dave!
+          Hello world (deployed with Travis)!
         </p>
         <a
           className="App-link"
