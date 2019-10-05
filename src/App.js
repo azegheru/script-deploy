37369060f0be20c1a08dbe99719961c4c89d3f6d
ubuntu user <ubuntu@AZEGHERU-RO.ro.oracle.com>
2019-10-05 20:10:21 +0300

    Modified app.js again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world again (deployed with Travis)!
+          Hello world! (deployed with Travis)!
         </p>
         <a
           className="App-link"
