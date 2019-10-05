4eb5d42199fe45aadd8b1f2737409b4dfbc081b1
ubuntu user <ubuntu@AZEGHERU-RO.ro.oracle.com>
2019-10-05 18:54:27 +0300

    Final commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing feature branch!
+          Hello world! (deployed with Travis CI)
         </p>
         <a
           className="App-link"
